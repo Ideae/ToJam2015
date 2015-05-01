02c0782027aacccb242917f59cb742527d8f6a36
Ideae <zack.tibia@gmail.com>
2015-05-01 19:44:56 -0400

    ok

---
+++ Assets/Bacteria.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bacteria : Animal {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public static void MoveRandom(GameObject go)
+    {
+
+    }
+}
