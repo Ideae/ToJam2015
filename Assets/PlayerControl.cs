bd124359e54cb20351c5bfb1d398ce36e204b5b6
Ideae <zack.tibia@gmail.com>
2015-05-01 20:43:22 -0400

    bacteria

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.collider != null)
+            {
+                Bacteria bact = hit.collider.GetComponent<Bacteria>();
+                if (bact != null)
+                {
+                    Bacteria.Divide(bact.gameObject);
+                }
+            }
+        }
+    }
+}
